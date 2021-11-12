eaea4db3a4f31cea7dfd0f589b062a14f1b01a61
tetyanakorolyuk <korolyuk11@gmail.com>
2021-11-12 12:35:26 +0200

    add js-hw-07

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+
+const imagesList = document.querySelector('.gallery');
+const imagesMarkup = createImagesList(galleryItems);
+
+imagesList.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createImagesList(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            title="${description}"
+          />
+        </a>`;
+    }).join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
